7d663ff5e7755cafbbff557826f672a43b20cc27
Zoltan Fraknoi <zfr@one.com>
2025-01-27 18:56:16 +0100

    getting bookings in a fixed period working

--- seatsurfing/booking.py
+++ seatsurfing/booking.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import json
 import logging
 from seatsurfing.common.http_client import SeatsurfingHttpClient
 
@@ -7,13 +8,13 @@ logger = logging.getLogger(__name__)
 
 class Booking(SeatsurfingHttpClient):
     def get_own_booking(self, booking_id: str):
-        r = self.__get(f"/booking/{booking_id}")
+        r = self._get(f"/booking/{booking_id}")
         logger.debug(r)
 
     def get_filtered_org_bookings(self, from_date: datetime, to_date: datetime):
         from_date_str = from_date.strftime("%Y-%m-%dT%H:%M:%SZ")
         to_date_str = to_date.strftime("%Y-%m-%dT%H:%M:%SZ")
         data = {"start": from_date_str, "end": to_date_str}
-        r = self.__post("/booking/filter/", data=data)
+        r = self._post("/booking/filter/", data=data)
         logger.debug(r)
         return r
--- seatsurfing/models/authentication.py
+++ seatsurfing/models/authentication.py
@@ -1,14 +1,15 @@
+from typing import Optional
 from pydantic import BaseModel, Field
 
 
 class PasswordLoginRequest(BaseModel):
     email: str
     password: str
-    long_lived: bool = Field(alias="longLived", default=True)
+    long_lived: Optional[bool] = Field(alias="longLived", default=True)
 
 
 class Jwt(BaseModel):
     access_token: str = Field(alias="accessToken")
     refresh_token: str = Field(alias="refreshToken")
-    long_lived: str = Field(alias="longLived")
-    logout_url: str = Field(alias="logoutUrl")
+    long_lived: bool = Field(alias="longLived")
+    logout_url: Optional[str] = Field(alias="logoutUrl")
