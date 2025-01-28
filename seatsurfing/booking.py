7d220c848feb82c332935a3aa3883056a28f76d7
Zoltan Fraknoi <zfr@one.com>
2025-01-28 08:57:57 +0100

    pydantic model for filtered booking

--- seatsurfing/booking.py
+++ seatsurfing/booking.py
@@ -2,6 +2,7 @@ from datetime import datetime
 import json
 import logging
 from seatsurfing.common.http_client import SeatsurfingHttpClient
+from seatsurfing.models.booking import Booking as BookingModel
 
 logger = logging.getLogger(__name__)
 
@@ -11,10 +12,17 @@ class Booking(SeatsurfingHttpClient):
         r = self._get(f"/booking/{booking_id}")
         logger.debug(r)
 
-    def get_filtered_org_bookings(self, from_date: datetime, to_date: datetime):
+    def get_filtered_org_bookings(
+        self, from_date: datetime, to_date: datetime
+    ) -> list[BookingModel]:
+        """
+        Get filtered org bookings.
+
+        Currently supports time period.
+        """
         from_date_str = from_date.strftime("%Y-%m-%dT%H:%M:%SZ")
         to_date_str = to_date.strftime("%Y-%m-%dT%H:%M:%SZ")
         data = {"start": from_date_str, "end": to_date_str}
         r = self._post("/booking/filter/", data=data)
-        logger.debug(r)
-        return r
+
+        return [BookingModel(**x) for x in r]
--- seatsurfing/models/booking.py
+++ seatsurfing/models/booking.py
@@ -1,5 +1,37 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+
+
+class Location(BaseModel):
+    id: str
+    organizationId: str
+    map_width: int = Field(alias="mapWidth")
+    map_height: int = Field(alias="mapHeight")
+    map_mime_type: str = Field(alias="mapMimeType")
+    name: str
+    description: str
+    max_concurrent_booking: int = Field(alias="maxConcurrentBookings")
+    timezone: str
+    enabled: bool
+
+
+class Space(BaseModel):
+    id: str
+    available: bool
+    location_id: str = Field(alias="locationId")
+    location: Location
+    name: str
+    x: int
+    y: int
+    width: int
+    height: int
+    rotation: int
 
 
 class Booking(BaseModel):
-    Id: str
+    id: str
+    user_id: str = Field(alias="userId")
+    user_email: str = Field(alias="userEmail")
+    space: Space
+    space_id: str = Field(alias="spaceId")
+    enter: str
+    leave: str
